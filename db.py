b7500e634fef7b91bce6f64df021af3a55714004
Karan <softwarewala22@gmail.com>
2026-03-29 23:24:41 +0530

    Fixed merge conflict in db.py

--- db.py
+++ db.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sqlite3
 
 DB_NAME = "ip_cache.db"
@@ -6,13 +5,4 @@ DB_NAME = "ip_cache.db"
 def get_connection():
     conn = sqlite3.connect(DB_NAME)
     conn.row_factory = sqlite3.Row
-=======
-import sqlite3
-
-DB_NAME = "ip_cache.db"
-
-def get_connection():
-    conn = sqlite3.connect(DB_NAME)
-    conn.row_factory = sqlite3.Row
->>>>>>> 88376c2dc64b4668dff9880171e5ff3d85432dd0
     return conn
